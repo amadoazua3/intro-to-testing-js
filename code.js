93cc7884f1b9517304b4d8313c500836d846f682
Amado Azua III <amado.azua3@gmail.com>
2021-04-24 17:49:53 -0500

    Refactoring exercise 11

--- code.js
+++ code.js
@@ -39,7 +39,37 @@ function isFive(input){
 
     if(input === "5"){
         return true;
-    }else{
+    }
+    else{
         return false;
     }
 }
+
+
+function isEven(input){
+
+
+    if(typeof input === "undefined") {
+        console.log("hello");
+        return false;
+    }
+    // if(input !== "undefined"){
+    //     console.log("YOU DEFINITELY SHOULDN'T BE HERE=");
+    //     return Boolean;
+    // }
+    if(input === true || input === false){
+        console.log("I am here");
+        return false;
+    }
+
+    if(input % 2 === 0){
+        console.log("you shouldn't be here");
+        return true;
+    }
+    if(input % 2 !== 0){
+        console.log("hmm so weird its rather odd");
+        return false;
+    }
+
+}
+
